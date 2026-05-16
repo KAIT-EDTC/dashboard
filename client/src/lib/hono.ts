aff16a55f84463990a3adbc4acc7f819f6f6628e
fami-gb <fami.gb0825@gmail.com>
2026-05-16 17:05:49 +0900

    fix(cookie): tokenを読み込めるように

--- client/src/lib/hono.ts
+++ client/src/lib/hono.ts
@@ -2,4 +2,8 @@ import { hc } from 'hono/client'
 import type { AppType } from '../../../server/src/index'
 
 //const API_BASE_URL = env.SERVER_URL || 'http://localhost:8787/'
-export const client = hc<AppType>('http://localhost:8788')
+export const client = hc<AppType>('http://localhost:8787', {
+  init: {
+    credentials: 'include',
+  }
+})
